75b527201f9cdcdb06712abd890805749c5f5f38
Jawve <jawvep@gmail.com>
2025-11-29 12:42:59 -0500

    testing functions and adding these functions to the toolset for the AI to use

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,20 +1,21 @@
 import os
-import functions.config as config
+from config import MAX_CHARS
+
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    target_fil = os.path.abspath(os.path.join(working_directory, file_path))
-    print(abs_working_dir)
-    print(target_fil)
-    if not target_fil.startswith(abs_working_dir):
-        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(target_fil):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    
-
-    with open(target_fil, "r") as f:
-        file_content_string = f.read(config.MAX_CHARS)
-        if config.MAX_CHARS == len(file_content_string):
-            file_content_string = file_content_string+ f'...File "{target_fil}" truncated at 10000 characters'
-        return file_content_string
-    
+    try:
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
